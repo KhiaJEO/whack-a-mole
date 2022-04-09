ce9394d64300ea4ee0093e9574dcff47ebb3d118
KhiaJEO <khiajeo@gmail.com>
2022-04-09 13:01:39 +0800

    adding randomiser for "I am mole"
    Remember that google is your friend

---
+++ src/Components/Mole.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Mole() {
+  const top = Math.floor(Math.random() * 400) + 1;
+  const left = Math.floor(Math.random() * 800) + 1;
+
+  return (
+    <button style={{ position: "absolute", top: top, left: left }}>
+      I am mole
+    </button>
+  );
+}
+
+export default Mole;
